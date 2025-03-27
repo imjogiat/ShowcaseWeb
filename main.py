652f7a051542245112a0f7a8538315be22065048
Ismail <imjogiat@gmail.com>
2025-03-27 16:07:03 -0600

    imported pandas to extract and use csv file data

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import PIL.Image
 import streamlit as stlt
 import PIL
 import os
+from pathlib import Path
+import pandas
 
 stlt.set_page_config(layout="wide")
 
@@ -12,11 +15,36 @@ with col1:
 with col2:
     stlt.title("Ismail Jogiat")
     aboutme = """ 
-                Hello, I'm Ismail. I'm just starting my journey in learning Python development and programming. 
-                I am eager to continue this journey and share the projects that I've worked on and developed along the way.
+                Hello, I'm Ismail. I'm just starting my journey in learning 
+                Python development and programming. 
+                I am eager to continue this journey and share the projects that 
+                I've worked on and developed along the way.
                 I will update this page as I work through various projects. 
                 """
     stlt.info(aboutme)
+
+statement1 = """
+                Below you can find some of the Apps that I have built using Python. 
+                Feel free to contact me for any details or if you have any questions!
+                """
+
+stlt.write(statement1)
+
+col3, col4 = stlt.columns(2)
+
+df = pandas.read_csv("data.csv", sep=";")
+
+with col3:
+    for index, data_row in df[:10].iterrows():
+        stlt.header(data_row["title"])
+    
+
+with col4:
+    for index, data_row in df[10:].iterrows():
+        stlt.header(data_row["title"])
+                
+                
+
 
 
 
